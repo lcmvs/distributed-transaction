2c61997ba997cc1ef3631d4ddebe158bee6a798f
longchenming <1264221821@qq.com>
2020-06-26 14:14:02 +0800

    init

---
+++ seata-test/src/main/java/com/lcm/learn/seatatest/SeataTestApplication.java
@@ -0,0 +1,13 @@
+package com.lcm.learn.seatatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeataTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeataTestApplication.class, args);
+    }
+
+}
---
+++ seata-test/src/main/java/com/lcm/learn/seatatest/dao/OrderDao.java
@@ -0,0 +1,26 @@
+package com.lcm.learn.seatatest.dao;
+
+
+import com.baomidou.dynamic.datasource.annotation.DS;
+import com.lcm.learn.seatatest.entity.OrderDO;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
+import org.springframework.stereotype.Repository;
+
+@Mapper
+@Repository
+
+public interface OrderDao {
+
+    /**
+     * 插入订单记录
+     *
+     * @param order 订单
+     * @return 影响记录数量
+     */
+    @Insert("INSERT INTO orders (user_id, product_id, pay_amount) VALUES (#{userId}, #{productId}, #{payAmount})")
+    @Options(useGeneratedKeys = true, keyColumn = "id", keyProperty = "id")
+    int saveOrder(OrderDO order);
+
+}
